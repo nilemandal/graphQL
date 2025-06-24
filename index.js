65dc52295d98abfc4bff0a6ba834c27bdff5b51a
Nilesh Mandal <nilemandal@yahoo.com>
2025-06-24 15:30:05 +0530

    feat: enhance GraphQL schema and resolvers to include relationships for reviews, games, and authors

--- index.js
+++ index.js
@@ -23,6 +23,24 @@ const resolvers = {
         author(_, args) {
             return db.authors.find(author => author.id === parseInt(args.id));
         }
+    },
+    Game: {
+        reviews(parent) {
+            return db.reviews.filter((review) => review.game_id === parent.id);
+        }
+    },
+    Author: {
+        reviews(parent) {
+            return db.reviews.filter((review) => review.author_id === parent.id);
+        }
+    },
+    Review: {
+        author(parent) {
+            return db.authors.find(author => author.id === parent.author_id);
+        },
+        game(parent) {
+            return db.games.find(game => game.id === parent.game_id);
+        }
     }
 };
 
